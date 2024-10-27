69d43a86c7b21814463d4f5779353b72db0f33ab
Nikola Dendic <ndendic@gmail.com>
2024-10-27 18:32:04 +0100

    Initial commit

--- project/app/components/auth/__initi__.py
+++
@@ -1,4 +0,0 @@
-from .login import login_page
-from .registration import registration_page
-
-__all__ = ["login_page", "registration_page"]
--- project/app/pages/index.py
+++ project/app/pages/index.py
@@ -1,5 +1,4 @@
 from fasthtml.common import *
-from app.components.home import home as Home
 from app.components.landing.page import landing_page
 from fasthtml.core import APIRouter
 import logging
@@ -13,9 +12,5 @@ rt = APIRouter()
 
 @rt("/")
 def get(request):
-    user = request.session.get("user")
-    if not user:
-        return landing_page()
-    user = request.session["user"]
-    # logger.info(f"Handling home request for user:{user}")
-    return Home(user)
+    return landing_page()
+
